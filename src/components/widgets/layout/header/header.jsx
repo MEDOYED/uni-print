7274852fc2adec59bcb4975148c88df33672b2d1
MEDOYED <mmax26072005@gmail.com>
2025-02-09 14:51:05 +0100

    toggle theme inside header

--- src/components/widgets/layout/header/header.jsx
+++ src/components/widgets/layout/header/header.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+
 import logoIcon from "../../../../assets/img/logo.png";
+import toggleThemeIcon from "../../../../assets/img/toggle-theme.svg";
 
 import "./header.scss";
 
 const Header = () => {
+  const [theme, setTheme] = useState("dark-theme");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "dark-theme" ? "light-theme" : "dark-theme"));
+  };
   return (
     <>
-      <header>
+      <header className={theme}>
         <img src={logoIcon} alt="" />
-        <button className="header__theme-toggle" type="button" aria-label="Переключення теми">
-          <img src="" alt="" />
+        <button
+          className="header__theme-toggle"
+          onClick={toggleTheme}
+          type="button"
+          aria-label="Переключення теми">
+          <img src={toggleThemeIcon} alt="" />
         </button>
         <div className="header__burger"></div>
         <img className="header__instagram-icon" src="" alt="" />
