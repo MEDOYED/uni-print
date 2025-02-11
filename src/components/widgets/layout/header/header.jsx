4822736ce7d158dea9427d20dec6dbd42ee295fc
MEDOYED <mmax26072005@gmail.com>
2025-02-11 11:32:53 +0100

    fix(header-burger-icon): support for dark and light theme

--- src/components/widgets/layout/header/header.jsx
+++ src/components/widgets/layout/header/header.jsx
@@ -14,8 +14,23 @@ const Header = () => {
 
         <ToggleTheme />
 
-        <button className="header__burger" role="button">
-          <span></span>
+        <button className="header__burger open" role="button">
+          {/* <span></span> */}
+          <svg
+            width="35"
+            height="35"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            // className="lucide lucide-menu">
+            className="header__burger-svg">
+            <line className="top" x1="4" x2="20" y1="12" y2="12" />
+            <line className="middle" x1="4" x2="20" y1="6" y2="6" />
+            <line className="bottom" x1="4" x2="20" y1="18" y2="18" />
+          </svg>
         </button>
 
         <Instagram className="header__instagram-icon" />
