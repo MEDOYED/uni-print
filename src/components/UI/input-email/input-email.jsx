9f4c0e9afc5a09ad05ed4a24d768bb4d4e8879af
MEDOYED <mmax26072005@gmail.com>
2025-02-15 04:55:37 +0100

    layout(InputEmail) | add placeholder

--- src/components/UI/input-email/input-email.jsx
+++ src/components/UI/input-email/input-email.jsx
@@ -3,7 +3,7 @@ import "./input-email.scss";
 const InputEmail = () => {
   return (
     <>
-      <input type="email" className="input-email" name="email" id="email" />
+      <input type="email" className="input-email" name="email" id="email" placeholder="E-mail" />
     </>
   );
 };
