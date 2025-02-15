9f5afee2446f2e793391814b08d2a1ecb3692f9e
MEDOYED <mmax26072005@gmail.com>
2025-02-16 00:18:29 +0100

    feat(WriteUsPopup): add light theme version

--- src/components/widgets/modal-window/write-us-popup/write-us-popup.jsx
+++ src/components/widgets/modal-window/write-us-popup/write-us-popup.jsx
@@ -1,4 +1,5 @@
 import { X } from "lucide-react";
+import useThemeStore from "../../../../store/themeStore";
 
 import InputName from "../../../UI/input-name/input-name";
 import InputEmail from "../../../UI/input-email/input-email";
@@ -7,23 +8,46 @@ import BtnSend from "../../../UI/btn-send/btn-send";
 import "./write-us-popup.scss";
 
 const WriteUsPopup = ({ isOpen, onClick }) => {
+  const theme = useThemeStore((state) => state.theme);
+  console.log(theme);
+
   if (!isOpen) return null;
-  return (
-    <>
-      <div className="write-us-popup">
-        <div className="write-us-popup__content">
-          <button onClick={onClick} className="close-window">
-            <X />
-          </button>
-          <form className="form" action="">
-            <InputName />
-            <InputEmail />
-            <BtnSend text={"Надіслати"} />
-          </form>
+
+  if (theme === "dark")
+    return (
+      <>
+        <div className="write-us-popup">
+          <div className="write-us-popup__content">
+            <button onClick={onClick} className="close-window">
+              <X />
+            </button>
+            <form className="form" action="">
+              <InputName />
+              <InputEmail />
+              <BtnSend text={"Надіслати"} />
+            </form>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+
+  if (theme === "light")
+    return (
+      <>
+        <div className="write-us-popup light">
+          <div className="write-us-popup__content light">
+            <button onClick={onClick} className="close-window">
+              <X />
+            </button>
+            <form className="form" action="">
+              <InputName />
+              <InputEmail />
+              <BtnSend text={"Надіслати"} />
+            </form>
+          </div>
+        </div>
+      </>
+    );
 };
 
 export default WriteUsPopup;
