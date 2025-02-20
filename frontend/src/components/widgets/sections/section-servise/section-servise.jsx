d590e22bcfd1f2d1b0922f4469a0a768751336bf
MEDOYED <mmax26072005@gmail.com>
2025-02-20 20:26:58 +0100

    chore(SectionServise): change text

--- frontend/src/components/widgets/sections/section-servise/section-servise.jsx
+++ frontend/src/components/widgets/sections/section-servise/section-servise.jsx
@@ -2,21 +2,21 @@ import { ALargeSmall } from "lucide-react";
 
 import CardServise from "../../card/card-servise/card-servise";
 
+import BrendAuto from "../../../../assets/img/Brenduvannya-auto.png";
+
 import "./section-servise.scss";
 
 const SectionServise = () => {
   return (
     <>
       <ul className="section-servise">
-        <CardServise text="Об'ємні літери" Icon={ALargeSmall} />
-        <CardServise text="Об'ємні літери" Icon={ALargeSmall} />
-        <CardServise text="Об'ємні літери" Icon={ALargeSmall} />
-        <CardServise text="Об'ємні літери" Icon={ALargeSmall} />
-        <CardServise text="Об'ємні літери" Icon={ALargeSmall} />
-        <CardServise text="Об'ємні літери" Icon={ALargeSmall} />
-        <CardServise text="Об'ємні літери" Icon={ALargeSmall} />
-        <CardServise text="Об'ємні літери" Icon={ALargeSmall} />
-        <CardServise text="Об'ємні літери" Icon={ALargeSmall} />
+        <CardServise text="Виготовлення вивісок" Icon={ALargeSmall} />
+        <CardServise text="Широкоформатний друк" Icon={ALargeSmall} />
+        <CardServise text="Брендування автотранспорту" Icon={ALargeSmall} />
+        <CardServise text="Оформлення вітрин" Icon={ALargeSmall} />
+        <CardServise text="Картини на холсті" Icon={ALargeSmall} />
+        <CardServise text="Поліграфічна продукція" Icon={ALargeSmall} />
+        <CardServise text="послуги дизайнера" Icon={ALargeSmall} />
       </ul>
     </>
   );
