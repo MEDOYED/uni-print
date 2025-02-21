6f5899b8ef8c54cc3f61e80a2124720a6029d398
MEDOYED <mmax26072005@gmail.com>
2025-02-21 22:37:40 +0100

    feat: change photos in ProjectsSection

--- frontend/src/components/widgets/sections/projects-section/projects-section.jsx
+++ frontend/src/components/widgets/sections/projects-section/projects-section.jsx
@@ -1,8 +1,13 @@
-import { MoveRight } from "lucide-react";
-
 import CardProjects from "../../card/card-projects/card-projects";
 
-import kaktus from "../../../../assets/img/kaktus.jpeg";
+import blackCar from "../../../../assets/img/projects/black-car.jpg";
+import eskulab from "../../../../assets/img/projects/eskulab.jpg";
+import babyBoss from "../../../../assets/img/projects/baby-boss.jpg";
+import ProffessionalCosmetics from "../../../../assets/img/projects/professional-cosmetics.jpg";
+import shoco from "../../../../assets/img/projects/shoco.jpg";
+import somersby from "../../../../assets/img/projects/somersby.png";
+import stand from "../../../../assets/img/projects/stand.jpg";
+import whiteCar from "../../../../assets/img/projects/white-car.jpg";
 
 import "./projects-section.scss";
 
@@ -11,14 +16,14 @@ const ProjectsSection = () => {
     <>
       <section className="projects-section">
         <ul className="list">
-          <CardProjects src={kaktus} />
-          <CardProjects src={kaktus} />
-          <CardProjects src={kaktus} />
-          <CardProjects src={kaktus} />
-          <CardProjects src={kaktus} />
-          <CardProjects src={kaktus} />
-          <CardProjects src={kaktus} />
-          <CardProjects src={kaktus} />
+          <CardProjects src={blackCar} />
+          <CardProjects src={eskulab} />
+          <CardProjects src={babyBoss} />
+          <CardProjects src={ProffessionalCosmetics} />
+          <CardProjects src={shoco} />
+          <CardProjects src={somersby} />
+          <CardProjects src={stand} />
+          <CardProjects src={whiteCar} />
         </ul>
       </section>
     </>
