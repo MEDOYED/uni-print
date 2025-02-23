fe4ad4af3f379352846211b8adeae522b9c9123a
Stanislaw-K <nightelf2293@gmail.com>
2025-02-23 22:15:31 +0100

    fix(BurgerMenu): increase z-index

--- frontend/src/components/widgets/modal-window/burger-menu/burger-menu.jsx
+++ frontend/src/components/widgets/modal-window/burger-menu/burger-menu.jsx
@@ -1,27 +1,33 @@
+import { createPortal } from "react-dom";
 import "./burger-menu.scss";
 
 const BurgerMenu = ({ isOpen }) => {
 
     const menuClassName = isOpen ? "burger-menu--open" : "burger-menu";
-    return (
-        <div className={menuClassName}>
-            <div className="line"></div>
-            <ul className="list">
+    return createPortal(
 
-                <li className="list__item-language">
-                    <span>Укр</span>
-                    <span>Рус</span>
-                </li>
+        <div className={`burger-menu ${isOpen ? "burger-menu--open" : ""}`}>
 
-                <li className="list__item">ГЛАВНАЯ</li>
-                <li className="list__item">ПРОЕКТЫ</li>
-                <li className="list__item">УСЛУГИ</li>
-                <li className="list__item">О НАС</li>
-                <li className="list__item">НОВОСТИ</li>
-                <li className="list__item">FAQ</li>
-                <li className="list__item">КОНТАКТЫ</li>
-            </ul>
-        </div>
+            <div className={menuClassName}>
+                <div className="line"></div>
+                <ul className="list">
+
+                    <li className="list__item-language">
+                        <span>Укр</span>
+                        <span>Рус</span>
+                    </li>
+
+                    <li className="list__item">ГЛАВНАЯ</li>
+                    <li className="list__item">ПРОЕКТЫ</li>
+                    <li className="list__item">УСЛУГИ</li>
+                    <li className="list__item">О НАС</li>
+                    <li className="list__item">НОВОСТИ</li>
+                    <li className="list__item">FAQ</li>
+                    <li className="list__item">КОНТАКТЫ</li>
+                </ul>
+            </div>
+        </div>,
+        document.getElementById("burger-root")
     );
 };
 
