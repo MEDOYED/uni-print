0ef4f3a07feafccb1b324e455354d39662d16a2d
MEDOYED <mmax26072005@gmail.com>
2025-02-24 22:43:40 +0100

    refactor: extract phone button in Heading into a separate file

---
+++ frontend/src/components/UI/buttons/btn-tel/btn-phone.jsx
@@ -0,0 +1,13 @@
+import "./btn-phone.scss";
+
+const BtnPhone = () => {
+  return (
+    <>
+      <a href="tel:+380983194257" className="btn-phone">
+        +38 (098) 319 42 57
+      </a>
+    </>
+  );
+};
+
+export default BtnPhone;
