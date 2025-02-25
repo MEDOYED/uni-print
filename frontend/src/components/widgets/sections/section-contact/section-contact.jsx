c1c82f96a3f92ffb1c1616c3ad842ddaf5ecc9ec
MEDOYED <mmax26072005@gmail.com>
2025-02-25 13:46:12 +0100

    feat: style(SectionContact)

--- frontend/src/components/widgets/sections/section-contact/section-contact.jsx
+++ frontend/src/components/widgets/sections/section-contact/section-contact.jsx
@@ -1,7 +1,9 @@
+import Lottie from "lottie-react";
+
 import BtnPhone from "../../../UI/buttons/btn-tel/btn-phone";
 import GreenButton from "../../../UI/green-button/green-button";
 
-import planeAnimation from "../../../../assets/video/plane-animation.webm";
+import planeAnimation from "../../../../assets/video/plane-animation.json";
 
 import "./section-contact.scss";
 
@@ -9,15 +11,20 @@ const SectionContact = () => {
   return (
     <>
       <section className="section-contact">
-        <video className="video" autoPlay loop muted playsInline>
-          <source src={planeAnimation} type="video/webm" />
-        </video>
+        <Lottie
+          className="video"
+          animationData={planeAnimation}
+          loop
+          muted
+        />
         <div className="content">
-          <h2 className="content-heading"></h2>
-          <span>Телефонуйте</span>
+          <h2 className="content__heading">
+            Залишились питання, чи потрібна консультація?
+          </h2>
+          <span className="content__text">Телефонуйте нам</span>
           <BtnPhone />
-          <span>Пишіть нам</span>
-          <BtnPhone />
+          <span className="content__text">Пишіть нам</span>
+          <BtnPhone margin="0px 0px 18px 0px" />
           <GreenButton text="Заповніть форму" />
         </div>
       </section>
