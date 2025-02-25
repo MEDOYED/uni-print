f499e5e522fbc30d4479d12c123598f611a7d171
MEDOYED <mmax26072005@gmail.com>
2025-02-25 14:29:07 +0100

    feat(SectionContact): connect modal window - WriteUpPopUp

--- frontend/src/components/widgets/sections/section-contact/section-contact.jsx
+++ frontend/src/components/widgets/sections/section-contact/section-contact.jsx
@@ -1,14 +1,20 @@
 import Lottie from "lottie-react";
+import { useState } from "react";
 
 import BtnPhone from "../../../UI/buttons/btn-tel/btn-phone";
 import GreenButton from "../../../UI/green-button/green-button";
 import ButtonEmail from "../../../UI/buttons/button-email/button-email";
+import WriteUsPopup from "../../modal-window/write-us-popup/write-us-popup";
 
 import planeAnimation from "../../../../assets/video/plane-animation.json";
 
 import "./section-contact.scss";
 
 const SectionContact = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openPopUp = () => setIsOpen(true);
+  const closePopup = () => setIsOpen(false);
   return (
     <>
       <section className="section-contact">
@@ -26,7 +32,8 @@ const SectionContact = () => {
           <BtnPhone />
           <span className="content__text">Пишіть нам</span>
           <ButtonEmail margin="0px 0px 50px 0px" />
-          <GreenButton text="Заповніть форму" />
+          <GreenButton onClick={openPopUp} text="Заповніть форму" />
+          <WriteUsPopup isOpen={isOpen} onClick={closePopup} />
         </div>
       </section>
     </>
