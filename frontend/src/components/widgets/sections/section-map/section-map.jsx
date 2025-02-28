7a0667a43181a3b33f0736e50ebed5c227da3cbb
MEDOYED <mmax26072005@gmail.com>
2025-02-28 01:17:01 +0100

    feat(SectionMap): connect map from leaflet

--- frontend/src/components/widgets/sections/section-map/section-map.jsx
+++ frontend/src/components/widgets/sections/section-map/section-map.jsx
@@ -1,9 +1,34 @@
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+} from "react-leaflet";
+
 import "./section-map.scss";
 
 const SectionMap = () => {
+  const latitude = 49.85746999061118;
+  const longitude = 24.038230710903044;
+  const zoom = 13;
+  const s = "a"; // subdomain
+
   return (
     <>
-      <div>test text</div>
+      <MapContainer
+        center={[latitude, longitude]}
+        zoom={zoom}
+        style={{ height: "400px", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+
+        <Marker position={[latitude, longitude]}>
+          <Popup>Test</Popup>
+        </Marker>
+      </MapContainer>
     </>
   );
 };
