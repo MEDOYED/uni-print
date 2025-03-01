cb9e9ae905b357605ad5ee80b419400e863edbb2
MEDOYED <mmax26072005@gmail.com>
2025-03-01 23:35:03 +0100

    layout(CardServiseDescr)

--- frontend/src/components/widgets/card/card-servise-desc/card-servise-desc.jsx
+++ frontend/src/components/widgets/card/card-servise-desc/card-servise-desc.jsx
@@ -3,7 +3,15 @@ import "./card-servise-desc.scss";
 const CardServiseDesc = () => {
   return (
     <>
-      <div>nfndvni</div>
+      <div className="card-servise-desc">
+        <h2 className="heading">Об'ємні літери</h2>
+        <p className="text">
+          Люди, які створюють візуальні об’єкти, намагаються досягти
+          максимальної реалістичності сприйняття об’єкту. Оскільки
+          людина сприймає візуальну інформацію у тривимірному форматі,
+          тому...
+        </p>
+      </div>
     </>
   );
 };
