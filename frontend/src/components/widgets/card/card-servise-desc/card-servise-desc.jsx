46d39a44a5af96f9af1351def6189e7803c2c349
MEDOYED <mmax26072005@gmail.com>
2025-03-02 12:41:54 +0100

    style: SectionServiseDesc

--- frontend/src/components/widgets/card/card-servise-desc/card-servise-desc.jsx
+++ frontend/src/components/widgets/card/card-servise-desc/card-servise-desc.jsx
@@ -6,8 +6,8 @@ const CardServiseDesc = () => {
   return (
     <>
       <div className="card-servise-desc">
-        <h2 className="heading">Об'ємні літери</h2>
-        <p className="text">
+        <h2 className="card-servise-desc__heading">Об'ємні літери</h2>
+        <p className="card-servise-desc__text">
           Люди, які створюють візуальні об’єкти, намагаються досягти
           максимальної реалістичності сприйняття об’єкту. Оскільки
           людина сприймає візуальну інформацію у тривимірному форматі,
