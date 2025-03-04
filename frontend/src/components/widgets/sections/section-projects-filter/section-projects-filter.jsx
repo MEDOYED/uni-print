374527555379d00517c50752d645ed914b52d73b
MEDOYED <mmax26072005@gmail.com>
2025-03-04 10:56:28 +0100

    refactor(SectionProjectsFilter): improve layout from static to .map

--- frontend/src/components/widgets/sections/section-projects-filter/section-projects-filter.jsx
+++ frontend/src/components/widgets/sections/section-projects-filter/section-projects-filter.jsx
@@ -3,10 +3,23 @@ import { useState } from "react";
 import "./section-projects-filter.scss";
 
 const SectionProjectsFilter = () => {
-  const [activeBtn, setActiveBtn] = useState(false);
+  const [activeBtn, setActiveBtn] = useState(null);
 
-  const handleChangeState = () => {
+  const filters = [
+    "Всі проекти",
+    "Виготовлення вивісок",
+    "Брендування авто",
+    "Друк фотозон",
+    "Рекламні конструкції і таблички",
+    "Широкоформатний друк",
+    "Друк картин на холсті",
+    "Оформлення вітрин",
+    "Поліграфія",
+  ];
+
+  const handleChangeState = index => {
     setActiveBtn(prevState => !prevState);
+    console.log(index);
   };
 
   const classNameItemBtn = activeBtn
@@ -16,51 +29,25 @@ const SectionProjectsFilter = () => {
   return (
     <>
       <ul className="section-projects-filter__btn-list">
-        <li className={classNameItemBtn} onClick={handleChangeState}>
-          <button className="section-projects-filter__btn">
-            Всі проекти
-          </button>
-        </li>
-        <li className={classNameItemBtn} onClick={handleChangeState}>
-          <button className="section-projects-filter__btn">
-            Виготовлення вивісок
-          </button>
-        </li>
-        <li className={classNameItemBtn} onClick={handleChangeState}>
-          <button className="section-projects-filter__btn">
-            Брендування авто
-          </button>
-        </li>
-        <li className={classNameItemBtn} onClick={handleChangeState}>
-          <button className="section-projects-filter__btn">
-            Друк фотозон
-          </button>
-        </li>
-        <li className={classNameItemBtn} onClick={handleChangeState}>
-          <button className="section-projects-filter__btn">
-            Рекламні конструкції і таблички
-          </button>
-        </li>
-        <li className={classNameItemBtn} onClick={handleChangeState}>
-          <button className="section-projects-filter__btn">
-            Широкоформатний друк
-          </button>
-        </li>
-        <li className={classNameItemBtn} onClick={handleChangeState}>
-          <button className="section-projects-filter__btn">
-            Друк картин на холсті
-          </button>
-        </li>
-        <li className={classNameItemBtn} onClick={handleChangeState}>
-          <button className="section-projects-filter__btn">
-            Оформлення вітрин
-          </button>
-        </li>
-        <li className={classNameItemBtn} onClick={handleChangeState}>
-          <button className="section-projects-filter__btn">
-            Поліграфія
-          </button>
-        </li>
+        {filters.map((elem, index) => {
+          return (
+            <>
+              <li
+                className={classNameItemBtn}
+                onClick={handleChangeState}
+                key={index}
+              >
+                <button
+                  onClick={() => handleChangeState(index)}
+                  className="section-projects-filter__btn"
+                >
+                  {elem}
+                </button>
+              </li>
+              ;
+            </>
+          );
+        })}
       </ul>
     </>
   );
