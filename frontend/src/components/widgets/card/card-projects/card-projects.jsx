d933d6f96773c60a549c372a8d84d43c443d8ad1
MEDOYED <mmax26072005@gmail.com>
2025-03-08 13:26:34 +0100

    fix: add links on HomePage to page projects in card-projects in SectionProjects

--- frontend/src/components/widgets/card/card-projects/card-projects.jsx
+++ frontend/src/components/widgets/card/card-projects/card-projects.jsx
@@ -1,8 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
 import ButtonReadMore from "../../../UI/buttons/button-read-more/button-read-more";
 
 import "./card-projects.scss";
 
 const CardProjects = ({ src, description }) => {
+  const navigate = useNavigate();
   return (
     <>
       <li className="card-projects__item">
@@ -12,7 +15,7 @@ const CardProjects = ({ src, description }) => {
           onClick={() => navigate("/projects")}
         >
           <div className="description">{description}</div>
-          <ButtonReadMore />
+          <ButtonReadMore link="/projects" />
         </div>
       </li>
     </>
