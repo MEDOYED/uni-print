ae84a23225778eee0053342fd2dbffd04e417921
MEDOYED <mmax26072005@gmail.com>
2025-03-08 14:14:00 +0100

    style(InputName):style for light theme

--- frontend/src/components/UI/input-name/input-name.jsx
+++ frontend/src/components/UI/input-name/input-name.jsx
@@ -1,15 +1,24 @@
+import useThemeStore from "../../../store/themeStore";
+
 import "./input-name.scss";
 
 const InputName = ({ value, onChange }) => {
+  const theme = useThemeStore(state => state.theme);
+  console.log("4444444444");
+  console.log(theme);
+  console.log("4444444444");
+
+  const className =
+    theme === "light" ? "input-name light" : "input-name";
   return (
     <>
       <input
-        className="input-name"
+        className={className}
         type="text"
         id="name"
         name="name"
         value={value}
-        onChange={(e) => onChange(e.target.value)}
+        onChange={e => onChange(e.target.value)}
         placeholder="Ваше ім'я"
         required
       />
