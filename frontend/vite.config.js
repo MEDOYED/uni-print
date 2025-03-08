3b905104f2d34ba9f8f30bde2e189600aedf6b2f
MEDOYED <mmax26072005@gmail.com>
2025-03-08 11:42:53 +0100

    chore & fix: fixed terminal warning

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "/src/styles/_variables.scss";`,
+        additionalData: `@use "/src/styles/_variables.scss" as *; `,
       },
     },
   },
