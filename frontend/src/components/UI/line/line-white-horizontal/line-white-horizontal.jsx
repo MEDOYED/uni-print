b3edad3c1c714363fad4b7772548b9c1aedc5761
MEDOYED <mmax26072005@gmail.com>
2025-03-10 10:13:04 +0100

    style(LineHorizontalLine): style for light theme

--- frontend/src/components/UI/line/line-white-horizontal/line-white-horizontal.jsx
+++ frontend/src/components/UI/line/line-white-horizontal/line-white-horizontal.jsx
@@ -1,9 +1,20 @@
+import useThemeStore from "../../../../store/themeStore";
+
 import "./line-white-horizontal.scss";
 
 const LineWhiteHorizontal = () => {
+  const theme = useThemeStore(state => state.theme);
+
+  console.log("aaaaa" + theme);
+
+  const className =
+    theme === "light"
+      ? "line-white-horizontal dark"
+      : "line-white-horizontal";
+
   return (
     <>
-      <hr className="line-white-horizontal" />
+      <hr className={className} />
     </>
   );
 };
