c25bbeea0d610b9f7eec8821095b27df949ba127
MEDOYED <mmax26072005@gmail.com>
2025-03-11 12:28:36 +0100

    chore: delete unused components

--- frontend/src/components/pages/home-page/home-page.jsx
+++ frontend/src/components/pages/home-page/home-page.jsx
@@ -2,7 +2,6 @@ import Heading from "../../widgets/layout/heading/heading";
 import SectionProjects from "../../widgets/sections/section-projects/section-projects";
 import TitleToSection from "../../widgets/layout/title-to-section/title-to-section";
 import SectionServise from "../../widgets/sections/section-servise/section-servise";
-import AboutUs from "../../widgets/sections/about-us-section/about-us-section";
 import LineWhiteHorizontal from "../../UI/line/line-white-horizontal/line-white-horizontal";
 import SectionContact from "../../widgets/sections/section-contact/section-contact";
 import Footer from "../../widgets/layout/footer/footer";
@@ -26,7 +25,6 @@ const HomePage = () => {
           link="/services"
         />
         <SectionServise />
-        <AboutUs />
 
         <LineWhiteHorizontal />
         <SectionContact
