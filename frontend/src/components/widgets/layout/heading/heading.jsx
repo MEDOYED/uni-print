a9c728789fd9ec8dee314182953629f712c40834
MEDOYED <mmax26072005@gmail.com>
2025-03-12 14:42:02 +0100

    feat(Heading): add logic when IconViber is render

--- frontend/src/components/widgets/layout/heading/heading.jsx
+++ frontend/src/components/widgets/layout/heading/heading.jsx
@@ -1,20 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import BtnPhone from "../../../UI/buttons/btn-phone/btn-phone";
 import GreenButton from "../../../UI/green-button/green-button";
 import WriteUsPopup from "../../modal-window/write-us-popup/write-us-popup";
+import IconViber from "../../../UI/svg/icon-viber/icon-viber";
 
 import "./heading.scss";
 
+const vwOnFirstRender = window.visualViewport.width;
+
 const Heading = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [viewportWidth, setViewportWidth] = useState(vwOnFirstRender);
 
   const openWindow = () => setIsOpen(true);
   const closePopup = () => setIsOpen(false);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportWidth(window.visualViewport.width);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
       <div className="heading">
+        {viewportWidth < 651 && (
+          <a
+            href="https://www.instagram.com/uniprint_lviv?igsh=amRlMGp5NjB5b3Vl"
+            target="_blank"
+          >
+            <IconViber />
+          </a>
+        )}
         <BtnPhone />
         <GreenButton onClick={openWindow} text={"Написати нам"} />
         <WriteUsPopup isOpen={isOpen} onClick={closePopup} />
