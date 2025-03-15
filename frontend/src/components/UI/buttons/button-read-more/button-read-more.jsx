718c2c38fe3c0c44c050a84aea80255688b2359b
MEDOYED <mmax26072005@gmail.com>
2025-03-15 19:44:40 +0100

    feat: read more text in CardServiseDesc functionality

--- frontend/src/components/UI/buttons/button-read-more/button-read-more.jsx
+++ frontend/src/components/UI/buttons/button-read-more/button-read-more.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 import "./button-read-more.scss";
 
-const ButtonReadMore = () => {
+const ButtonReadMore = ({ onToggle }) => {
+  const [isActive, setIsActive] = useState(true);
+
+  const onHandleClick = () => {
+    setIsActive(prev => !prev);
+
+    if (onToggle) {
+      onToggle(isActive);
+    }
+  };
+
   return (
     <>
-      <button className="button-read-more">Читати далі</button>
+      <button className="button-read-more" onClick={onHandleClick}>
+        Читати далі
+      </button>
     </>
   );
 };
--- frontend/src/components/widgets/card/card-servise-desc/card-servise-desc.jsx
+++ frontend/src/components/widgets/card/card-servise-desc/card-servise-desc.jsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
+
 import ButtonDetails from "../../../UI/buttons/button-details/button-details";
 import ButtonReadMore from "../../../UI/buttons/button-read-more/button-read-more";
 
 import "./card-servise-desc.scss";
 
 const CardServiseDesc = ({ heading, text, link }) => {
+  const [isButtonReadMoreIsActive, setIsButtonReadMoreIsActive] =
+    useState(false);
+
+  const handleActive = newIsActive => {
+    setIsButtonReadMoreIsActive(newIsActive);
+  };
+
+  const textClassName =
+    isButtonReadMoreIsActive === true
+      ? "card-servise-desc__text--active"
+      : "card-servise-desc__text";
+
   return (
     <>
       <div className="card-servise-desc">
         <h2 className="card-servise-desc__heading">{heading}</h2>
-        <p className="card-servise-desc__text">{text}</p>
+        <p className={textClassName}>{text}</p>
         <ButtonDetails link={link} />
-        <ButtonReadMore />
+        <ButtonReadMore onToggle={handleActive} />
       </div>
     </>
   );
